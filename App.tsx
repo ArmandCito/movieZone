7151e34fb75381678e7669f72fea62b52b0a0408
ArmandCito <armandcito@gmail.com>
2026-08-20 20:55:10 +0200

    Project initialisation

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import IntroScreen from './src/screens/IntroScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import MovieDetailScreen from './src/screens/MovieDetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <StatusBar style="light" />
+      <Stack.Navigator
+        initialRouteName="Intro"
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: '#121212' },
+        }}
+      >
+        <Stack.Screen name="Intro" component={IntroScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="MovieDetail" component={MovieDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
